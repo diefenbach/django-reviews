1e84f1f0d9f9f2b4404df79740a2fab357a562b7
Kai Diefenbach <kai.diefenbach@iqpp.de>
2009-10-11 10:32:18 +0200

    Added metadata

---
+++ README.txt
@@ -0,0 +1,7 @@
+Django Reviews 
+==============
+
+Generic reviews for Django.
+
+Documention will follow. In the meanwhile you can see a comprehensive 
+implementation within LFS: http://bitbucket.org/diefenbach/django-lfs/
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+setup(name='django-reviews',
+      version=version,
+      description='Generic reviews for Django.',
+      long_description=README,
+      classifiers=[
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ],
+      keywords='django reviews',
+      author='Kai Diefenbach',
+      author_email='kai.diefenbach@iqpp.de',
+      url='http://www.iqpp.com',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
+      )
