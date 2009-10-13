f1e1470cc44bde675bfdc9f73a116c637ee173bf
Kai Diefenbach <kai.diefenbach@iqpp.de>
2009-10-13 21:31:27 +0200

    Added ``get_best_rated`` and ``get_best_rated_for_model`` methods.

--- reviews/utils.py
+++ reviews/utils.py
@@ -6,6 +6,44 @@ from django.db import connection
 # review imports
 from reviews.models import Review
 
+def get_best_rated():
+    """Returns the best rated instance at all.
+    """
+    cursor = connection.cursor()
+    cursor.execute("""SELECT avg(score), content_type_id, content_id
+                      FROM reviews_review
+                      WHERE active=%s
+                      GROUP BY content_id
+                      ORDER BY score DESC""", [True])
+
+    try:
+        score, content_type_id, content_id = cursor.fetchone()
+        ctype = ContentType.objects.get_for_id(content_type_id)
+        content = ctype.model_class().objects.get(pk=content_id)
+        return content, score
+    except (TypeError, ObjectDoesNotExist):
+        return None
+
+def get_best_rated_for_model(instance):
+    """Returns the best rated instance for given model or instance of a model.
+    """
+    ctype = ContentType.objects.get_for_model(instance)
+
+    cursor = connection.cursor()
+    cursor.execute("""SELECT avg(score), content_id
+                      FROM reviews_review
+                      WHERE content_type_id=%s
+                      AND active=%s
+                      GROUP BY content_id
+                      ORDER BY score DESC""", [ctype.id, True])
+
+    try:
+        score, content_id = cursor.fetchone()
+        content = ctype.model_class().objects.get(pk=content_id)
+        return content, score
+    except (TypeError, ObjectDoesNotExist):
+        return None
+
 def get_reviews_for_instance(instance):
     """Returns active reviews for given instance.
     """
@@ -15,7 +53,7 @@ def get_reviews_for_instance(instance):
 def get_average_for_instance(instance):
     """Returns the average score and the amount of reviews for the given
     instance. Takes only active reviews into account.
-    
+
     Returns (average, amount)
     """
     # TODO: Check Django 1.1's aggregation
